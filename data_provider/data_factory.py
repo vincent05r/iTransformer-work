3c7d9403d00771e36710bbf2003025f3f9511df2
Vincent Zhu <111105725+vincent05r@users.noreply.github.com>
2024-12-22 22:06:35 +1100

    Update data_factory.py

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -48,7 +48,9 @@ def data_provider(args, flag):
         timeenc=timeenc,
         freq=freq,
     )
+    
     print(flag, len(data_set))
+
     data_loader = DataLoader(
         data_set,
         batch_size=batch_size,

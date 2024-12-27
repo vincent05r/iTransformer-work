24e1329683410f117110fe2ec772760fc851fbda
Vincent Zhu <111105725+vincent05r@users.noreply.github.com>
2024-12-27 22:59:04 +1100

    update

--- scripts/JST_v1/iTf_v2.sh
+++ scripts/JST_v1/iTf_v2.sh
@@ -18,10 +18,10 @@ python -u run.py \
   --label_len 0 \
   --pred_len 1 \
   --c_out 1 \
-  --d_model 64 \
-  --d_ff 64 \
-  --n_heads 4 \
-  --e_layers 2 \
+  --d_model 8 \
+  --d_ff 128 \
+  --n_heads 2 \
+  --e_layers 4 \
   --dropout 0.1 \
   --embed fixed \
   --train_epochs 50 \
@@ -30,4 +30,4 @@ python -u run.py \
   --learning_rate 0.001 \
   --lradj typeh_d \
   --des 'JST_V1_t1' \
-  --itr 1 > test3.txt
+  --itr 1 > test_s2.txt
